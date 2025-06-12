4e620389ab284a957683081230f3114f80b59735
Vishal Bachal <100130993+vishal2807@users.noreply.github.com>
2025-06-12 18:21:19 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src') // Maps `@/` to `src/`
+    }
+  },
   preview: {
-    // Allow your Render domain in Vite's preview server
     allowedHosts: ['vishal-data-insights-hub.onrender.com']
   },
   server: {
-    // Ensure the server binds to all interfaces
     host: '0.0.0.0',
-    port: parseInt(process.env.PORT || '5000') // Use Render's assigned port
+    port: parseInt(process.env.PORT || '5000')
   }
 })
